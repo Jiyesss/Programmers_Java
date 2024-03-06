499b85e64b97782fe90c8cd0a6d90cb970d45e4a
Jiyesss <choijiyea7185@gmail.com>
2024-03-06 13:13:02 +0900

    1주차(2/6)

---
+++ src/Lv0/Lv0_각도기.java
@@ -0,0 +1,18 @@
+package Lv0;
+
+public class Lv0_각도기 {
+    class Solution {
+        public int solution(int angle) {
+            int answer;
+            if (angle > 0 && angle < 90)
+                answer = 1;
+            else if (angle == 90)
+                answer = 2;
+            else if (angle >90 && angle < 180)
+                answer = 3;
+            else
+                answer = 4;
+            return answer;
+        }
+    }
+}
