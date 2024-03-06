0f041c5e7e2dc53ccd6aed9bb4789bf9d5d75c49
Jiyesss <choijiyea7185@gmail.com>
2024-03-06 13:14:37 +0900

    1주차(1/6)

---
+++ src/Lv0/Lv0_몫구하기.java
@@ -0,0 +1,15 @@
+package Lv0;
+
+
+public class Lv0_몫구하기 {
+    public static void main(String[] args) {
+
+
+        class Solution {
+            public int solution(int num1, int num2) {
+                int answer = num1 / num2;
+                return answer;
+            }
+        }
+    }
+}
