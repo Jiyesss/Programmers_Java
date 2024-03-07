c031ed2ae4f19b579800774c588a1e35920d767d
Jiyesss <122000486+Jiyesss@users.noreply.github.com>
2024-03-07 14:35:27 +0900

    1주차(3/6)

---
+++ src/Lv0/Lv0_짝수의_합.java
@@ -0,0 +1,21 @@
+package Lv0;
+
+public class Lv0_짝수의_합 {
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            while (n > 0)
+                if (n % 2 == 0) {
+                    answer += n;
+                    n -= 2;
+                }
+                else {
+                    n -= 1;
+                    answer += n;
+                    n -= 2;
+                }
+
+            return answer;
+        }
+    }
+}
