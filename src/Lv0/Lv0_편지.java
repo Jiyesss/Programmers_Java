8e11219544a7fca984128c09971889872cebecb9
Jiyesss <choijiyea7185@gmail.com>
2024-03-17 20:26:52 +0900

    2주차 (6/6)

---
+++ src/Lv0/Lv0_편지.java
@@ -0,0 +1,11 @@
+package Lv0;
+
+public class Lv0_편지 {
+    class Solution {
+        public int solution(String message) {
+            int answer = 0;
+            answer = message.length() * 2;
+            return answer;
+        }
+    }
+}
