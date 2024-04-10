70cac9cd1c28493396f6d5b6ddd37a99dc9c7363
ziyesss <choijiyea7185@gmail.com>
2024-04-10 17:05:34 +0900

    6주차 (1/3)

---
+++ src/Lv0/Lv0_중앙값_구하기.java
@@ -0,0 +1,14 @@
+package Lv0;
+
+public class Lv0_중앙값_구하기 {
+    import java.util.*;
+
+    class Solution {
+        public int solution(int[] array) {
+
+            Arrays.sort(array);
+
+            return array[array.length/2];
+        }
+    }
+}
