78631269364cf0d343d2511fe78f2b67e2c4b9c2
ziyesss <choijiyea7185@gmail.com>
2024-05-12 17:00:04 +0900

    7주차 (2/3)

---
+++ src/Lv0/Lv0_짝수홀수개수.java
@@ -0,0 +1,17 @@
+package Lv0;
+
+public class Lv0_짝수홀수개수 {
+    class Solution {
+        public int[] solution(int[] num_list) {
+            int[] answer = new int[2];
+            for (int i: num_list) {
+                if (i % 2 == 0) {
+                    answer[0] += 1;
+                }
+                else answer[1] += 1;
+            }
+            return answer;
+        }
+    }
+
+}
